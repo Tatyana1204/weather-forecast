02d774844d143f0d29505bd4e74d67b01e2c99d4
Tatyana1204 <Smetanina2002sme@gmail.com>
2024-09-27 12:32:13 +0300

    create store and api-connection and search

---
+++ src/loader/loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './loader.module.css';
+const Loader = () => {
+    return (
+        <div className={classes.loader}>
+        </div>
+    );
+};
+
+export default Loader;
